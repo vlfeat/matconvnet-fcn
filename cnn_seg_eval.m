8854ceb6858712bf4e8e4cc133e24534e1eb031d
Andrea Vedaldi <vedaldi@gmail.com>
2015-05-18 23:35:35 +0100

    further useless tuning

--- cnn_seg_eval.m
+++ cnn_seg_eval.m
@@ -4,9 +4,9 @@ run ~/src/vlfeat/toolbox/vl_setup ;
 run matconvnet/matlab/vl_setupnn ;
 addpath matconvnet/examples ;
 
-opts.expDir = 'data/baseline-11' ;
+opts.expDir = 'data/baseline-12' ;
 opts.imdbPath = 'data/voc12/imdb-ext.mat';
-opts.modelPath = fullfile(opts.expDir, 'net-epoch-20.mat');
+opts.modelPath = fullfile(opts.expDir, 'net-epoch-60.mat');
 opts.dataDir = 'data/voc12' ;
 opts.vocEdition = '12' ;
 opts.numFetchThreads = 12 ;
